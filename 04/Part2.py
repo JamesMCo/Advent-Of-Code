4c0882a08cf5462cc0e487dcb42cc29be63ff8d2
James C <3661681+JamesMCo@users.noreply.github.com>
2015-12-14 19:11:51 +0000

    Completed day 4

---
+++ 04/Part2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 4, Part 2
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read().split("\n")[0]
+f.close()
+
+import hashlib
+
+i = 0
+while True:
+    current_hash = hashlib.md5(str(puzzle_input + str(i)).encode("utf-8")).hexdigest()
+    if current_hash[0:6] == "000000":
+        break
+    print("Tried " + str(i) + ", returned: " + current_hash)
+    i += 1
+
+print("Tried " + str(i) + ", returned: " + current_hash)
+print("The lowest positive integer to produce a hash is: " + str(i) + ".")
