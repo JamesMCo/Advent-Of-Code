7fe324e0991e25e9cec039dbc2aec4ee9c3c4759
James C <3661681+JamesMCo@users.noreply.github.com>
2015-12-14 19:59:32 +0000

    Incomplete (and wrong) Day 5, Part 1

---
+++ 05/Part1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 5, Part 1
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read().split("\n")
+f.close()
+
+nice = 0
+
+for i in puzzle_input[0:-1]:
+    vowels = 0
+    vowels += list(i).count("a")
+    vowels += list(i).count("e")
+    vowels += list(i).count("i")
+    vowels += list(i).count("o")
+    vowels += list(i).count("u")
+
+    doubles = False
+    for x in range(97, 122):
+        if chr(x)*2 in i:
+            doubles = True
+
+    badstring = False
+    for x in ["ab", "cd", "pq", "xy"]:
+        if x in i:
+            badstring = True
+
+    if vowels >= 3 and doubles == True and badstring == False:
+        nice += 1
+
+print("There are " + str(nice) + " nice strings.")
