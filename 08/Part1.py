e2e6fb55bcc7145638fa929721639f5acfce8c1d
James C <3661681+JamesMCo@users.noreply.github.com>
2015-12-16 22:38:21 +0000

    Completed day 8
    I'll get to a working solution for day 7 eventually...

---
+++ 08/Part1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 8, Part 1
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read().split("\n")
+f.close()
+
+literals = 0
+memory = 0
+
+for i in puzzle_input[0:-1]:
+    literals += len(i)
+    memory += len(eval(i))
+
+print("Characers of code in literal: " + str(literals))
+print("Charaters of code in memory:  " + str(memory))
+print("Literals - Memory = " + str(literals - memory))
---
+++ 08/Part2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 8, Part 2
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read().split("\n")
+f.close()
+
+literals = 0
+encoded = 0
+
+for i in puzzle_input[0:-1]:
+    literals += len(i)
+    encoded += len("\""+i.replace('"', '/"').replace("\\", "\\\\").replace("/", "\\")+"\"")
+
+print("Characers of code in encoding: " + str(encoded))
+print("Charaters of code in literals: " + str(literals))
+print("Encoded - Literals = " + str(encoded - literals))
