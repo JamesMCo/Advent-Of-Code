36406fb0bce5cce64c138acf15882f6fc9ac4813
James C <3661681+JamesMCo@users.noreply.github.com>
2015-12-26 17:06:08 +0000

    Completed day 10

---
+++ 10/Part2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 10, Part 2
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()
+f.close()
+
+import itertools
+
+def look_and_say(original, repeats):
+    current = original
+    for i in range(repeats):
+        split = ["".join(grp) for num, grp in itertools.groupby(current)]
+        new = ""
+        for x in split:
+            new += str(len(x))
+            new += x[0]
+        current = new
+    return(current)
+
+print("The final result is " + str(len(look_and_say(puzzle_input[0:-1], 50))) + " characters long.")
