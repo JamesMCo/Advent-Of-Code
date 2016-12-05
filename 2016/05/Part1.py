0c7c93583e48dc82889e7e5f1b00ff8abb00ea50
James C <3661681+JamesMCo@users.noreply.github.com>
2016-12-05 20:12:12 +0000

    Added 2016 Day 5

---
+++ 2016/05/Part1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 5, Part 1
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()[:-1]
+f.close()
+
+from hashlib import md5
+
+characters_found = 0
+password = ""
+current_index = 0
+
+while characters_found < 8:
+    current_hash = md5(str(puzzle_input + str(current_index)).encode("utf-8")).hexdigest()
+    if len(current_hash) > 5 and current_hash[:5] == "0"*5:
+        password += current_hash[5]
+        characters_found += 1
+    current_index += 1
+
+print("The password is " + password + ".")
---
+++ 2016/05/Part2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 5, Part 2
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()[:-1]
+f.close()
+
+from hashlib import md5
+from random import choice
+from string import ascii_letters, punctuation
+
+def printpass(replace=True):
+    if replace: print("\x1b[1A\x1b[2K", end="")
+    print("The password is ", end="")
+    for i in password:
+        if i != "_":
+            print(i, end="")
+        else:
+            print(choice(ascii_letters + punctuation), end="")
+    print(".")
+
+password = ["_" for i in range(8)]
+current_index = 0
+printpass(False)
+
+while "_" in password:
+    current_hash = md5(str(puzzle_input + str(current_index)).encode("utf-8")).hexdigest()
+    if len(current_hash) > 5 and current_hash[:5] == "0"*5 and 0 <= int(current_hash[5], 16) <= 7 and password[int(current_hash[5], 16)] == "_":
+        password[int(current_hash[5], 16)] = current_hash[6]
+        printpass()
+        current_index += 1
+        continue
+    if current_index % 15000 == 0:
+        printpass()
+    current_index += 1
