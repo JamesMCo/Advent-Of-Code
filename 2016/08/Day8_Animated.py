09b4b47fa28f58ab046c6d7c701b6f21beb45a25
James C <3661681+JamesMCo@users.noreply.github.com>
2016-12-08 23:20:25 +0000

    Made more of 2016 Day 8 animated

--- 2016/08/Day8_Animated.py
+++ 2016/08/Day8_Animated.py
@@ -19,33 +19,38 @@ def rect(s, d):
     for x in range(w):
         for y in range(h):
             d[y][x] = "#"
+            print("\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[2K", end="")
+            printd(d)
     return d
 
 def rotate(roc, n, a, d):
-    if roc == "row":
-        cur = "".join(d[int(n)]) + "".join(d[int(n)])
-        if int(a) > 0:
-            cur = cur[50-int(a):100-int(a)]
+    for i in range(int(a)):
+        if roc == "row":
+            cur = "".join(d[int(n)]) + "".join(d[int(n)])
+            if 1 > 0:
+                cur = cur[49:99]
+            else:
+                cur = cur[int(a):51]
+            d[int(n)] = list(cur)
         else:
-            cur = cur[int(a):50+int(a)]
-        d[int(n)] = list(cur)
-    else:
-        cur = ""
-        for row in d:
-            cur += row[int(n)]
-        cur += cur
-        if int(a) > 0:
-            cur = cur[6-int(a):12-int(a)]
-        else:
-            cur = cur[int(a):6+int(a)]
-        for p in range(len(cur)):
-            d[p][int(n)] = cur[p]
+            cur = ""
+            for row in d:
+                cur += row[int(n)]
+            cur += cur
+            if int(a) > 0:
+                cur = cur[5:11]
+            else:
+                cur = cur[int(a):7]
+            for p in range(len(cur)):
+                d[p][int(n)] = cur[p]
+        print("\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[2K", end="")
+        printd(d)
     return d
 
 def printd(d):
     for row in d:
         print("".join(row))
-    time.sleep(0.1)
+    time.sleep(0.05)
 
 print("\n\n\n\n\n")
 for inst in puzzle_input:
@@ -53,5 +58,3 @@ for inst in puzzle_input:
         d = rect(inst.split(" ")[1], d)
     elif inst.split(" ")[0] == "rotate":
         d = rotate(inst.split(" ")[1], inst.split(" ")[2].split("=")[1], inst.split(" ")[4], d)
-    print("\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[1A\x1b[2K", end="")
-    printd(d)
