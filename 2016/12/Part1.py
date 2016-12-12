231b6cf844578d2c1a947af974f56303d7f6f6be
James C <3661681+JamesMCo@users.noreply.github.com>
2016-12-12 16:19:16 +0000

    Added 2016 Day 12

---
+++ 2016/12/Part1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 12, Part 1
+#Solution by Jammy4312 (https://jammy4312.me)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()[:-1].split("\n")
+f.close()
+
+registers = {}
+
+i = 0
+while i < len(puzzle_input):
+    j = puzzle_input[i].split(" ")
+    if j[0] == "cpy":
+        try:
+            int(j[1])
+            registers[j[2]] = int(j[1])
+        except:
+            registers[j[2]] = registers[j[1]]
+    elif j[0] == "inc":
+        registers[j[1]] += 1
+    elif j[0] == "dec":
+        registers[j[1]] -= 1
+    elif j[0] == "jnz":
+        try:
+            int(j[1])
+            if j[1] != "0":
+                i += int(j[2]) - 1
+        except:
+            if j[1] in registers and registers[j[1]] != 0 and j[2] != 0:
+                i += int(j[2]) - 1
+    i += 1
+
+print("The value of register a is " + str(registers["a"]) + ".")
