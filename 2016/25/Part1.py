4074f57b1fd069fb13cf4f5d640880df0acc4b1e
James C <3661681+JamesMCo@users.noreply.github.com>
2017-11-05 00:25:16 +0000

    Added 2016 Day 25

---
+++ 2016/25/Part1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 25, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()[:-1].split("\n")
+f.close()
+
+lowest = 0
+breakout = False
+
+while not breakout:
+    registers = {"a": lowest}
+    output = ""
+
+    print("New run: a initialised to " + str(registers["a"]))
+
+    i = 0
+    k = 0
+    while i < len(puzzle_input) and k < 20 and not breakout:
+        j = puzzle_input[i].split(" ")
+        if j[0] == "cpy":
+            try:
+                int(j[1])
+                registers[j[2]] = int(j[1])
+            except:
+                registers[j[2]] = registers[j[1]]
+        elif j[0] == "inc":
+            registers[j[1]] += 1
+        elif j[0] == "dec":
+            registers[j[1]] -= 1
+        elif j[0] == "jnz":
+            try:
+                int(j[1])
+                if j[1] != "0":
+                    try:
+                        int(j[2])
+                        i += int(j[2]) - 1
+                    except:
+                        i += registers[j[2]] - 1
+            except:
+                if j[1] in registers and registers[j[1]] != 0 and j[2] != 0:
+                    i += int(j[2]) - 1
+        elif j[0] == "out":
+            try:
+                int(j[1])
+                print(int(j[1]), end="")
+                output += str(int(j[1]))
+            except:
+                print(registers[j[1]], end="")
+                output += str(registers[j[1]])
+            if output[-15:] == "101010101010101" or output[-15:] == "010101010101010":
+                breakout = True
+            k += 1
+        i += 1
+
+    print("\n")
+    if not breakout:
+        lowest += 1
+
+print("The lowest initialisation of register a that causes a clock is " + str(lowest) + ".")
