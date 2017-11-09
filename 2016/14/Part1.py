d224d3c3b29c0bf963d8ea0ef362f4c02f28da8a
James C <3661681+JamesMCo@users.noreply.github.com>
2017-11-09 20:17:33 +0000

    Added 2016 Day 14

---
+++ 2016/14/Part1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 14, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()[:-1]
+f.close()
+
+from hashlib import md5
+
+keys = 0
+i = 0
+
+hashes = {}
+
+def hash(s):
+    global hashes
+
+    if s not in hashes:
+        hashes[s] = md5(str(s).encode("utf-8")).hexdigest()
+    return hashes[s]
+
+while keys < 64:
+    current_hash = hash(puzzle_input + str(i))
+    for c in range(len(current_hash)):
+        try:
+            if current_hash[c] == current_hash[c+1] == current_hash[c+2]:
+                for j in range(1, 1001):
+                    if current_hash[c]*5 in hash(puzzle_input + str(i+j)):
+                        keys += 1
+                        break
+                break
+        except:
+            pass
+    i += 1
+
+i -= 1
+print("The index that generates the 64th key is " + str(i) + ".")
