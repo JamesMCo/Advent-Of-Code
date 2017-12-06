0022e311da4b8933adcf024d383697581568118b
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-06 19:13:11 +0000

    Updated 2017 test script to use os-specific execution commands

--- 2017/test.py
+++ 2017/test.py
@@ -1,16 +1,21 @@
 import os
 
+if os.name == "nt":
+    s = ""
+else:
+    s = "./"
+
 for day in range(1, 25):
     if os.path.isdir(str(day).zfill(2)):
         os.chdir(str(day).zfill(2))
         if os.path.isfile("Part1.py"):
             print(f"*** Testing Day {day} Part 1 ***")
-            r = os.system("Part1.py")
+            r = os.system(s + "Part1.py")
             if r:
                 exit(r)
         if os.path.isfile("Part2.py"):
             print(f"*** Testing Day {day} Part 2 ***")
-            r = os.system("Part2.py")
+            r = os.system(s + "Part2.py")
             if r:
                 exit(r)
         os.chdir("../")
