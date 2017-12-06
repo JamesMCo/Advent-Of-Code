6ee56c028a1780fffbf604e677fab67ed398e377
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-06 11:52:00 +0000

    Optimised 2017 Day 6 Part 2

--- 2017/06/Part2.py
+++ 2017/06/Part2.py
@@ -8,21 +8,18 @@ f = open("puzzle_input.txt")
 puzzle_input = [int(x) for x in f.read()[:-1].split(" ") if x != ""]
 f.close()
 
+history = []
 length = len(puzzle_input)
-state = 2
 
-while state:
-    history = []
-    while str(puzzle_input) not in history:
-        history.append(str(puzzle_input))
-        i = j = puzzle_input.index(max(puzzle_input))
-        c = puzzle_input[i]
-        puzzle_input[j] = 0
-        j = (i + 1) % length
-        while c > 0:
-            puzzle_input[j] += 1
-            c -= 1
-            j = (j + 1) % length
-    state -= 1
+while str(puzzle_input) not in history:
+    history.append(str(puzzle_input))
+    i = j = puzzle_input.index(max(puzzle_input))
+    c = puzzle_input[i]
+    puzzle_input[j] = 0
+    j = (i + 1) % length
+    while c > 0:
+        puzzle_input[j] += 1
+        c -= 1
+        j = (j + 1) % length
 
-print("The number of redistribution cycles in the infinite loop is " + str(len(history)) + ".")
+print("The number of redistribution cycles in the infinite loop is " + str(len(history) - history.index(str(puzzle_input))) + ".")
