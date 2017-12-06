f4339bd0d388cba516b9239012c762e064e8b2ad
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-06 01:10:31 +0000

    Some optimisations to 2017 Day 5
     :musical_note: 30 million calls to int,
    Running in a while loop... :musical_note:

--- 2017/05/Part1.py
+++ 2017/05/Part1.py
@@ -5,14 +5,15 @@
 #Solution by James C. (https://github.com/JamesMCo)
 
 f = open("puzzle_input.txt")
-puzzle_input = f.read()[:-1].split("\n")
+puzzle_input = [int(x) for x in f.read()[:-1].split("\n")]
 f.close()
 
 i = 0
 steps = 0
+length = len(puzzle_input)
 
-while 0 <= i < len(puzzle_input):
-    t = int(puzzle_input[i])
+while 0 <= i < length:
+    t = puzzle_input[i]
     puzzle_input[i] = t + 1
     i += t
     steps += 1
--- 2017/05/Part2.py
+++ 2017/05/Part2.py
@@ -5,18 +5,19 @@
 #Solution by James C. (https://github.com/JamesMCo)
 
 f = open("puzzle_input.txt")
-puzzle_input = f.read()[:-1].split("\n")
+puzzle_input = [int(x) for x in f.read()[:-1].split("\n")]
 f.close()
 
 i = 0
 steps = 0
+length = len(puzzle_input)
 
-while 0 <= i < len(puzzle_input):
-    t = int(puzzle_input[i])
-    if t >= 3:
-        puzzle_input[i] = t - 1
+while 0 <= i < length:
+    t = puzzle_input[i]
+    if puzzle_input[i] >= 3:
+        puzzle_input[i] -= 1
     else:
-        puzzle_input[i] = t + 1
+        puzzle_input[i] += 1
     i += t
     steps += 1
 
