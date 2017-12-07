5fc03f5e9b62faf47081f329aa1bdffa4773f123
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-07 08:54:21 +0000

    Completed 2017 Day 7

---
+++ 2017/07/Part1.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 7, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import unittest
+
+programs = {}
+
+class Program:
+    def __init__(self, name, weight=None, children=[]):
+        global programs
+
+        self.name = name
+        self.weight = weight
+        self.children = children
+        self.parent = None
+
+        programs[name] = self
+
+    def getName(self):
+        return self.name
+
+    def setWeight(self, weight):
+        self.weight = weight
+
+    def getChildren(self):
+        return self.children
+
+    def setChildren(self, children):
+        self.children = children
+
+    def getParent(self):
+        return self.parent
+
+    def setParent(self, other):
+        self.parent = other
+
+    def __str__(self):
+        return "Program \"" + self.name + "\" | Weight = " + str(self.weight) + ", Children = " + str(self.children) + ", Parent = " + str(self.parent)
+
+def getProgramById(pName):
+    if pName in programs:
+        return programs[pName]
+    else:
+        return Program(pName)
+
+def solve(puzzle_input):
+    global programs
+
+    for l in puzzle_input:
+        w = l.split(" ")
+        p = getProgramById(w[0])
+        p.setWeight(int(w[1][1:-1]))
+        if len(w) > 2:
+            p.setChildren([x.replace(",", "") for x in w[3:]])
+
+    for p in programs:
+        if programs[p].getChildren() != []:
+            for c in programs[p].getChildren():
+                programs[c].setParent(p)
+
+    for p in programs:
+        if programs[p].getParent() == None:
+            return programs[p]
+
+def main():
+    f = open("puzzle_input.txt")
+    puzzle_input = f.read()[:-1].split("\n")
+    f.close()
+
+    base = solve(puzzle_input)
+
+    print("The name of the bottom program is " + str(base.getName()) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve(["pbga (66)", "xhth (57)", "ebii (61)",
+                                "havc (66)", "ktlj (57)", "fwft (72) -> ktlj, cntj, xhth",
+                                "qoyq (66)", "padx (45) -> pbga, havc, qoyq",
+                                "tknk (41) -> ugml, padx, fwft", "jptl (61)",
+                                "ugml (68) -> gyxo, ebii, jptl", "gyxo (61)",
+                                "cntj (57)"]).getName(), "tknk")
+
+    def tearDown(self):
+        global programs
+
+        for p in list(programs.keys()):
+            del programs[p]
+        programs = {}
+
+if __name__ == "__main__":
+    if unittest.main(verbosity=2, exit=False).result.wasSuccessful():
+        main()
+        exit(0)
+    else:
+        exit(1)
