d7e89cd266fa9706411510ae55fd56aab525e319
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-09 11:41:23 +0000

    Completed 2017 Day 9

---
+++ 2017/09/Part1.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 9, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import unittest
+
+def solve(puzzle_input):
+    depth = 0
+    total = 0
+
+    i = 0
+    while i < len(puzzle_input):
+        if puzzle_input[i] == "!":
+            puzzle_input = puzzle_input[:i] + puzzle_input[i+2:]
+        else:
+            i += 1        
+
+    i = 0
+    in_garbage = False
+    while i < len(puzzle_input):
+        if in_garbage:
+            if puzzle_input[i] == ">":
+                in_garbage = False
+            puzzle_input = puzzle_input[:i] + puzzle_input[i+1:]
+        elif puzzle_input[i] == "<":
+            in_garbage = True
+            puzzle_input = puzzle_input[:i] + puzzle_input[i+1:]
+        else:
+            i += 1
+
+    for i in puzzle_input:
+        if i == "{":
+            depth += 1
+            total += depth
+        elif i == "}":
+            if depth > 0:
+                depth -= 1
+
+    return total
+
+def main():
+    f = open("puzzle_input.txt")
+    puzzle_input = f.read()[:-1]
+    f.close()
+
+    score = solve(puzzle_input)
+
+    print("The total score for all groups in the input is " + str(score) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve("{}"), 1)
+
+    def test_ex2(self):
+        self.assertEqual(solve("{{{}}}"), 6)
+
+    def test_ex3(self):
+        self.assertEqual(solve("{{},{}}"), 5)
+
+    def test_ex4(self):
+        self.assertEqual(solve("{{{},{},{{}}}}"), 16)
+
+    def test_ex5(self):
+        self.assertEqual(solve("{<a>,<a>,<a>,<a>}"), 1)
+
+    def test_ex6(self):
+        self.assertEqual(solve("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9)
+
+    def test_ex7(self):
+        self.assertEqual(solve("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9)
+
+    def test_ex8(self):
+        self.assertEqual(solve("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3)
+
+if __name__ == "__main__":
+    if unittest.main(verbosity=2, exit=False).result.wasSuccessful():
+        main()
+        exit(0)
+    else:
+        exit(1)
---
+++ 2017/09/Part2.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 9, Part 2
+#Solution by James C. (https://github.com/JamesMCo)
+
+import unittest
+
+def solve(puzzle_input):
+    count = 0
+
+    i = 0
+    while i < len(puzzle_input):
+        if puzzle_input[i] == "!":
+            puzzle_input = puzzle_input[:i] + puzzle_input[i+2:]
+        else:
+            i += 1        
+
+    i = 0
+    in_garbage = False
+    while i < len(puzzle_input):
+        if in_garbage:
+            if puzzle_input[i] == ">":
+                in_garbage = False
+            else:
+                count += 1
+        elif puzzle_input[i] == "<":
+            in_garbage = True
+        i += 1
+
+    return count
+
+def main():
+    f = open("puzzle_input.txt")
+    puzzle_input = f.read()[:-1]
+    f.close()
+
+    count = solve(puzzle_input)
+
+    print("The number of non-cancelled charcters whithin the garbage is " + str(count) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve("<>"), 0)
+
+    def test_ex2(self):
+        self.assertEqual(solve("<random characters>"), 17)
+
+    def test_ex3(self):
+        self.assertEqual(solve("<<<<>"), 3)
+
+    def test_ex4(self):
+        self.assertEqual(solve("<{!>}>"), 2)
+
+    def test_ex5(self):
+        self.assertEqual(solve("<!!>"), 0)
+
+    def test_ex6(self):
+        self.assertEqual(solve("<!!!>>"), 0)
+
+    def test_ex7(self):
+        self.assertEqual(solve("<{o\"i!a,<{i<a>"), 10)
+
+if __name__ == "__main__":
+    if unittest.main(verbosity=2, exit=False).result.wasSuccessful():
+        main()
+        exit(0)
+    else:
+        exit(1)
