45400d6791732b3f095bee829e9f80a9f3c34907
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-17 22:07:26 +0000

    Completed 2016 Day 13 :star: :star2: :christmas_tree:

---
+++ 2016/13/Part1.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 13, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+f = open("puzzle_input.txt")
+puzzle_input = int(f.read()[:-1])
+f.close()
+
+from math import inf
+
+cx = 1
+cy = 1
+
+def getNeighbours(x, y):
+    neigbours = set()
+    if x != 0:
+        a = x-1
+        b = y
+        if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+            neigbours.add((a, b))
+    if y != 0:
+        a = x
+        b = y-1
+        if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+            neigbours.add((a, b))
+    a = x+1
+    b = y
+    if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+        neigbours.add((a, b))
+    a = x
+    b = y+1
+    if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+        neigbours.add((a, b))
+
+    return neigbours
+
+class Vertex:
+    def __init__(self, x, y, dist=inf):
+        self.x = x
+        self.y = y
+        self.dist = dist
+        self.prev = None
+
+    def __eq__(self, coords):
+        return self.x == coords[0] and self.y == coords[1]
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+def Disjkstra(Graph, source, dest):
+    Q = {}
+    All = {}
+
+    for v in Graph:
+        if v == source:
+            Q[(v[0], v[1])] = All[(v[0], v[1])] = Vertex(v[0], v[1], 0)
+        else:
+            Q[(v[0], v[1])] = All[(v[0], v[1])] = Vertex(v[0], v[1])
+
+    while Q != {}:
+        u = min(Q, key=lambda x: Q[x].dist)
+        Q.pop(u)
+        if u == dest:
+            break
+
+        for v in getNeighbours(u[0], u[1]):
+            if v in Q:
+                alt = All[u].dist + 1
+                if alt < Q[v].dist:
+                    Q[v].dist = alt
+                    Q[v].prev = u
+
+    return All[dest].dist
+
+graph = set()
+
+for y in range(50):
+    for x in range(50):
+        if (x, y) == (31, 39):
+            print("X", end="")
+            graph.add((x, y))
+            continue
+        if bin(x*x + 3*x + 2*x*y + y + y*y + puzzle_input).count("1") % 2 == 1:
+            print("#", end="")
+        else:
+            print(".", end="")
+            graph.add((x, y))
+    print()
+
+
+print("The fewest number of steps required to reach (31, 39) is " + str(Disjkstra(graph, (1, 1), (31, 39))) + ".")
---
+++ 2016/13/Part2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 13, Part 2
+#Solution by James C. (https://github.com/JamesMCo)
+
+f = open("puzzle_input.txt")
+puzzle_input = int(f.read()[:-1])
+f.close()
+
+from math import inf
+
+cx = 1
+cy = 1
+
+def getNeighbours(x, y):
+    neigbours = set()
+    if x != 0:
+        a = x-1
+        b = y
+        if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+            neigbours.add((a, b))
+    if y != 0:
+        a = x
+        b = y-1
+        if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+            neigbours.add((a, b))
+    a = x+1
+    b = y
+    if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+        neigbours.add((a, b))
+    a = x
+    b = y+1
+    if bin(a*a + 3*a + 2*a*b + b + b*b + puzzle_input).count("1") % 2 == 0:
+        neigbours.add((a, b))
+
+    return neigbours
+
+class Vertex:
+    def __init__(self, x, y, dist=inf):
+        self.x = x
+        self.y = y
+        self.dist = dist
+        self.prev = None
+
+    def __eq__(self, coords):
+        return self.x == coords[0] and self.y == coords[1]
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+def ModifiedDisjkstra(Graph, source):
+    Q = {}
+    All = {}
+
+    for v in Graph:
+        if v == source:
+            Q[(v[0], v[1])] = All[(v[0], v[1])] = Vertex(v[0], v[1], 0)
+        else:
+            Q[(v[0], v[1])] = All[(v[0], v[1])] = Vertex(v[0], v[1])
+
+    while Q != {}:
+        u = min(Q, key=lambda x: Q[x].dist)
+        Q.pop(u)
+
+        for v in getNeighbours(u[0], u[1]):
+            if v in Q:
+                alt = All[u].dist + 1
+                if alt < Q[v].dist:
+                    Q[v].dist = alt
+                    Q[v].prev = u
+
+    count = 0
+    for coord in All:
+        if All[coord].dist <= 50:
+            count += 1
+    return count
+
+graph = set()
+
+for y in range(60):
+    for x in range(60):
+        if bin(x*x + 3*x + 2*x*y + y + y*y + puzzle_input).count("1") % 2 == 1:
+            print("#", end="")
+        else:
+            print(".", end="")
+            graph.add((x, y))
+    print()
+
+print("The number of coordinates that can be reached in 50 steps is " + str(ModifiedDisjkstra(graph, (1, 1))) + ".")
