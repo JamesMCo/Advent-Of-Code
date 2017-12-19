fac9d5b401e405d03e8b48d15197c6e72c2c020e
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-19 13:49:34 +0000

    2017 Day 19 improvements

--- 2017/19/Part2.py
+++ 2017/19/Part2.py
@@ -7,14 +7,14 @@
 import string, unittest
 
 def solve(puzzle_input):
-    steps = 1
     direction = "d"
-    cur_x = 0
-    cur_y = 0
+    steps  = 1
+    cur_x  = 0
+    cur_y  = 0
     prev_x = -1
     prev_y = -1
-    max_x = len(puzzle_input[0])
-    max_y = len(puzzle_input)
+    max_x  = len(puzzle_input[0])
+    max_y  = len(puzzle_input)
 
     spaces = "|-+" + string.ascii_uppercase
 
@@ -24,42 +24,34 @@ def solve(puzzle_input):
     while (cur_x, cur_y) != (prev_x, prev_y):
         prev_x, prev_y = cur_x, cur_y
 
-        if direction == "d":
+        if puzzle_input[cur_y][cur_x] == "+":
+            if direction in "ud":
+                if cur_x != max_x and puzzle_input[cur_y][cur_x+1] in spaces:
+                    cur_x += 1
+                    direction = "r"
+                elif cur_x != 0 and puzzle_input[cur_y][cur_x-1] in spaces:
+                    cur_x -= 1
+                    direction = "l"
+            else:
+                if cur_y != max_y and puzzle_input[cur_y+1][cur_x] in spaces:
+                    cur_y += 1
+                    direction = "d"
+                elif cur_y != 0 and puzzle_input[cur_y-1][cur_x] in spaces:
+                    cur_y -= 1
+                    direction = "u"
+        elif direction == "d":
             if cur_y != max_y and puzzle_input[cur_y+1][cur_x] in spaces:
                 cur_y += 1
-            elif cur_x != max_x and puzzle_input[cur_y][cur_x+1] in spaces:
-                cur_x += 1
-                direction = "r"
-            elif cur_x != 0 and puzzle_input[cur_y][cur_x-1] in spaces:
-                cur_x -= 1
-                direction = "l"
         elif direction == "u":
             if cur_y != 0 and puzzle_input[cur_y-1][cur_x] in spaces:
                 cur_y -= 1
-            elif cur_x != max_x and puzzle_input[cur_y][cur_x+1] in spaces:
-                cur_x += 1
-                direction = "r"
-            elif cur_x != 0 and puzzle_input[cur_y][cur_x-1] in spaces:
-                cur_x -= 1
-                direction = "l"
         elif direction == "l":
             if cur_x != 0 and puzzle_input[cur_y][cur_x-1] in spaces:
                 cur_x -= 1
-            elif cur_y != max_y and puzzle_input[cur_y+1][cur_x] in spaces:
-                cur_y += 1
-                direction = "d"
-            elif cur_y != 0 and puzzle_input[cur_y-1][cur_x] in spaces:
-                cur_y -= 1
-                direction = "u"
         elif direction == "r":
             if cur_x != max_x and puzzle_input[cur_y][cur_x+1] in spaces:
                 cur_x += 1
-            elif cur_y != max_y and puzzle_input[cur_y+1][cur_x] in spaces:
-                cur_y += 1
-                direction = "d"
-            elif cur_y != 0 and puzzle_input[cur_y-1][cur_x] in spaces:
-                cur_y -= 1
-                direction = "u"
+
         if (cur_x, cur_y) != (prev_x, prev_y):
             steps += 1
 
