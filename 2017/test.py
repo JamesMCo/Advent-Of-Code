2c8d9599916af8baa9cfe8af9c04ee1d772f2b00
James C <3661681+JamesMCo@users.noreply.github.com>
2017-12-26 01:47:51 +0000

    Added 2017 puzzle names to index page/tests

--- 2017/test.py
+++ 2017/test.py
@@ -9,18 +9,24 @@ if os.name == "nt":
 else:
     s = "python ./"
 
-sep = "*" * 28
+names = ["",
+         "Inverse Captcha", "Corruption Checksum", "Spiral Memory", "High-Entropy Passphrases", "A Maze of Twisty Trampolines, All Alike",
+         "Memory Reallocation", "Recursive Circus", "I Heard You Like Registers", "Stream Processing", "Knot Hash",
+         "Hex Ed", "Digital Plumber", "Packet Scanners", "Disk Defragmentation", "Dueling Generators",
+         "Permutation Promenade", "Spinlock", "Duet", "A Series of Tubes", "Particle Swarm",
+         "Fractal Art", "Sporifica Virus", "Coprocessor Conflagration", "Electromagnetic Moat", "The Halting Problem"]
 
 printed = False
+p_day   = False
 for day in range(1, 26):
-    if day == 10:
-        sep += "*"
     if os.path.isdir(str(day).zfill(2)):
         os.chdir(str(day).zfill(2))
         if os.path.isfile("Part1.py"):
             if printed:
                 print("\n")
-            print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Testing Day {day} Part 1  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n")
+            sep = "*" * len(f"**  Day {day}  - {names[day]}  **")
+            print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Day {day}  - {names[day]}  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n\n\n{colorama.Fore.GREEN}Testing Part 1{colorama.Fore.RESET}\n")
+            p_day = True
             
             r = os.system(s + "Part1.py")
             if r:
@@ -30,7 +36,11 @@ for day in range(1, 26):
         if os.path.isfile("Part2.py"):
             if printed:
                 print("\n")
-            print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Testing Day {day} Part 2  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n")
+            if not p_day:
+                sep = "*" * len(f"**  Day {day}  - {names[day]}  **")
+                print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Day {day}  - {names[day]}  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n\n\n{colorama.Fore.GREEN}Testing Part 2{colorama.Fore.RESET}\n")
+            else:
+                print(f"{colorama.Fore.GREEN}Testing Part 2{colorama.Fore.RESET}\n")
             
             r = os.system(s + "Part2.py")
             if r:
