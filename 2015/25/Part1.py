88e72238b572a046bf486b3fd761e15bba7a0498
James C <3661681+JamesMCo@users.noreply.github.com>
2018-02-27 19:34:05 +0000

    Solved 2015 Day 25

---
+++ 2015/25/Part1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 25, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+f = open("puzzle_input.txt")
+puzzle_input = f.read()[0:-1].split()
+f.close()
+
+row = int(puzzle_input[puzzle_input.index("row")+1][:-1])
+col = int(puzzle_input[puzzle_input.index("column")+1][:-1])
+codeno = 1 + sum(range(1, row)) + sum(range(row + 1, row + col))
+
+code = 20151125
+for i in range(codeno - 1):
+    code = (code * 252533) % 33554393
+
+print("The code from the manual is " + str(code) + ".")
