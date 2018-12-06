ec679d27b6f7b840d30ea6bc05ef235acbddf430
James C <3661681+JamesMCo@users.noreply.github.com>
2018-12-06 06:25:44 +0000

    Completed 2018 Day 6

---
+++ 2018/06/Part1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 6, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys, colorama, time
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.tests
+
+from collections import defaultdict
+
+def solve(puzzle_input):
+    def distance(a, b):
+        return abs(a[0]-b[0]) + abs(a[1]-b[1])
+
+    puzzle_input = [(int(x.split(", ")[0]), int(x.split(", ")[1])) for x in puzzle_input]
+    width  = max([x[0] for x in puzzle_input]) - min([x[0] for x in puzzle_input])
+    height = max([x[1] for x in puzzle_input]) - min([x[1] for x in puzzle_input])
+
+    left  = min([z[0] for z in puzzle_input]) - 10
+    right = min([z[0] for z in puzzle_input]) + width + 10
+    top   = min([z[1] for z in puzzle_input]) - 10
+    bot   = min([z[1] for z in puzzle_input]) + height + 10
+    grid = defaultdict(int)
+
+    #Distances for the grid, with a margin of 10 on all sides
+    for x in range(left, right+1):
+        for y in range(top, bot+1):
+            working = sorted([(z, distance(z, (x, y))) for z in puzzle_input], key=lambda a: a[1])
+            if [z[1] for z in working].count(working[0][1]) == 1:
+                grid[str(working[0][0])] += 1
+
+    #Discount areas that are infinite (closest to the borders)
+    for x in range(left, right+1):
+        for y in range(top, bot+1):
+            if y not in [top, bot] and x not in [left, right]:
+                continue
+
+            working = sorted([(z, distance(z, (x, y))) for z in puzzle_input], key=lambda a: a[1])
+            if [z[1] for z in working].count(working[0][1]) == 1:
+                grid[str(working[0][0])] = 0
+
+    return max(grid.values())
+
+def main():
+    f = open("puzzle_input.txt")
+    puzzle_input = f.read()[:-1].split("\n")
+    f.close()
+
+    area = solve(puzzle_input)
+
+    print("The size of the largest (non infinite) area is " + str(area) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve(["1, 1",
+                                "1, 6",
+                                "8, 3",
+                                "3, 4",
+                                "5, 5",
+                                "8, 9"]), 17)
+
+if __name__ == "__main__":
+    if unittest.main(verbosity=2, exit=False, testRunner=util.tests.Runner).result.wasSuccessful():
+        start = time.time()
+        main()
+        end = time.time()
+        print(f"{colorama.Fore.CYAN}Solution found in {colorama.Fore.GREEN}{round(end - start, 3)}s{colorama.Fore.CYAN}.{colorama.Fore.RESET}")
+        exit(0)
+    else:
+        exit(1)
---
+++ 2018/06/Part2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#Day 6, Part 2
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys, colorama, time
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.tests
+
+def solve(puzzle_input, max_sum=10_000):
+    def distance(a, b):
+        return abs(a[0]-b[0]) + abs(a[1]-b[1])
+
+    puzzle_input = [(int(x.split(", ")[0]), int(x.split(", ")[1])) for x in puzzle_input]
+
+    mid_x = int( ( min([z[0] for z in puzzle_input]) + max([z[0] for z in puzzle_input]) ) /2 )
+    mid_y = int( ( min([z[1] for z in puzzle_input]) + max([z[1] for z in puzzle_input]) ) /2 )
+
+    left_x = mid_x
+    while sum([distance(z, (left_x, mid_y)) for z in puzzle_input]) < max_sum:
+        left_x -= 1
+    left_x -= 10
+
+    right_x = mid_x
+    while sum([distance(z, (right_x, mid_y)) for z in puzzle_input]) < max_sum:
+        right_x += 1
+    right_x += 10
+
+    top_y = mid_y
+    while sum([distance(z, (mid_x, top_y)) for z in puzzle_input]) < max_sum:
+        top_y -= 1
+    top_y -= 10
+
+    bot_y = mid_y
+    while sum([distance(z, (mid_x, bot_y)) for z in puzzle_input]) < max_sum:
+        bot_y += 1
+    bot_y += 10
+
+    region = 0
+    for x in range(left_x, right_x+1):
+        for y in range(top_y, bot_y+1):
+            working = [distance(z, (x, y)) for z in puzzle_input]
+            if sum(working) < max_sum:
+                region += 1
+
+    return region
+
+def main():
+    f = open("puzzle_input.txt")
+    puzzle_input = f.read()[:-1].split("\n")
+    f.close()
+
+    area = solve(puzzle_input)
+
+    print("The size of the region is " + str(area) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve(["1, 1",
+                                "1, 6",
+                                "8, 3",
+                                "3, 4",
+                                "5, 5",
+                                "8, 9"], 32), 16)
+
+if __name__ == "__main__":
+    if unittest.main(verbosity=2, exit=False, testRunner=util.tests.Runner).result.wasSuccessful():
+        start = time.time()
+        main()
+        end = time.time()
+        print(f"{colorama.Fore.CYAN}Solution found in {colorama.Fore.GREEN}{round(end - start, 3)}s{colorama.Fore.CYAN}.{colorama.Fore.RESET}")
+        exit(0)
+    else:
+        exit(1)
