295aa464c28c8b4074d0e5c3859c88960d8b7633
James C <3661681+JamesMCo@users.noreply.github.com>
2018-12-19 16:14:08 +0000

    Completed 2018 Day 19

---
+++ 2018/19/Part1.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2018 Day 19, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    class CPU:
+        def __init__(self, program):
+            self.register = [0, 0, 0, 0, 0, 0]
+            self.ip       = 0
+            self.ipr      = None
+            self.instructions = {"addr": self.addr,
+                                 "addi": self.addi,
+                                 "mulr": self.mulr,
+                                 "muli": self.muli,
+                                 "banr": self.banr,
+                                 "bani": self.bani,
+                                 "borr": self.borr,
+                                 "bori": self.bori,
+                                 "setr": self.setr,
+                                 "seti": self.seti,
+                                 "gtir": self.gtir,
+                                 "gtri": self.gtri,
+                                 "gtrr": self.gtrr,
+                                 "eqir": self.eqir,
+                                 "eqri": self.eqri,
+                                 "eqrr": self.eqrr
+                                }
+            self.program = program
+
+        def bind_ip(self, r):
+            self.ipr = r
+
+        def run(self):
+            self.register[self.ipr] = self.ip
+
+            opcode, a, b, c = self.program[self.ip].split()
+            self.instructions[opcode](int(a), int(b), int(c))
+
+            self.ip = self.register[self.ipr]
+            self.ip += 1
+
+
+        def addr(self, a, b, c):
+            self.register[c] = self.register[a] + self.register[b]
+
+        def addi(self, a, b, c):
+            self.register[c] = self.register[a] + b
+
+        def mulr(self, a, b, c):
+            self.register[c] = self.register[a] * self.register[b]
+
+        def muli(self, a, b, c):
+            self.register[c] = self.register[a] * b
+
+        def banr(self, a, b, c):
+            self.register[c] = self.register[a] & self.register[b]
+
+        def bani(self, a, b, c):
+            self.register[c] = self.register[a] & b
+
+        def borr(self, a, b, c):
+            self.register[c] = self.register[a] | self.register[b]
+
+        def bori(self, a, b, c):
+            self.register[c] = self.register[a] | b
+
+        def setr(self, a, b, c):
+            self.register[c] = self.register[a]
+
+        def seti(self, a, b, c):
+            self.register[c] = a
+
+        def gtir(self, a, b, c):
+            if a > self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def gtri(self, a, b, c):
+            if self.register[a] > b:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def gtrr(self, a, b, c):
+            if self.register[a] > self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def eqir(self, a, b, c):
+            if a == self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def eqri(self, a, b, c):
+            if self.register[a] == b:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def eqrr(self, a, b, c):
+            if self.register[a] == self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+    cpu = CPU(puzzle_input[1:])
+    cpu.bind_ip(int(puzzle_input[0].split()[1]))
+    while 0 <= cpu.ip < len(cpu.program):
+        cpu.run()
+    return cpu.register[0]
+
+def main():
+    puzzle_input = util.read.as_lines()
+
+    value = solve(puzzle_input)
+
+    print("The value left in register 0 is " + str(value) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve(["#ip 0",
+                                "seti 5 0 1",
+                                "seti 6 0 2",
+                                "addi 0 1 0",
+                                "addr 1 2 3",
+                                "setr 1 0 0",
+                                "seti 8 0 4",
+                                "seti 9 0 5"]), 6)
+
+run(main)
