0e0593d857fb125cef77416849ab0f1456f9bc3d
James C <3661681+JamesMCo@users.noreply.github.com>
2018-12-21 21:32:08 +0000

    Completed 2018 Day 21

---
+++ 2018/21/Part1.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2018 Day 21, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    class CPU:
+        def __init__(self, program):
+            self.register = [0, 0, 0, 0, 0, 0]
+            self.ip       = 0
+            self.ipr      = None
+            self.instructions = {"addr": self.addr,
+                                 "addi": self.addi,
+                                 "mulr": self.mulr,
+                                 "muli": self.muli,
+                                 "banr": self.banr,
+                                 "bani": self.bani,
+                                 "borr": self.borr,
+                                 "bori": self.bori,
+                                 "setr": self.setr,
+                                 "seti": self.seti,
+                                 "gtir": self.gtir,
+                                 "gtri": self.gtri,
+                                 "gtrr": self.gtrr,
+                                 "eqir": self.eqir,
+                                 "eqri": self.eqri,
+                                 "eqrr": self.eqrr
+                                }
+            self.program = program
+            self.counter = 0
+
+        def bind_ip(self, r):
+            self.ipr = r
+
+        def run(self):
+            self.register[self.ipr] = self.ip
+
+            opcode, a, b, c = self.program[self.ip].split()
+            self.instructions[opcode](int(a), int(b), int(c))
+
+            self.ip = self.register[self.ipr]
+            self.ip += 1
+            self.counter += 1
+
+
+        def addr(self, a, b, c):
+            self.register[c] = self.register[a] + self.register[b]
+
+        def addi(self, a, b, c):
+            self.register[c] = self.register[a] + b
+
+        def mulr(self, a, b, c):
+            self.register[c] = self.register[a] * self.register[b]
+
+        def muli(self, a, b, c):
+            self.register[c] = self.register[a] * b
+
+        def banr(self, a, b, c):
+            self.register[c] = self.register[a] & self.register[b]
+
+        def bani(self, a, b, c):
+            self.register[c] = self.register[a] & b
+
+        def borr(self, a, b, c):
+            self.register[c] = self.register[a] | self.register[b]
+
+        def bori(self, a, b, c):
+            self.register[c] = self.register[a] | b
+
+        def setr(self, a, b, c):
+            self.register[c] = self.register[a]
+
+        def seti(self, a, b, c):
+            self.register[c] = a
+
+        def gtir(self, a, b, c):
+            if a > self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def gtri(self, a, b, c):
+            if self.register[a] > b:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def gtrr(self, a, b, c):
+            if self.register[a] > self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def eqir(self, a, b, c):
+            if a == self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def eqri(self, a, b, c):
+            if self.register[a] == b:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+        def eqrr(self, a, b, c):
+            if self.register[a] == self.register[b]:
+                self.register[c] = 1
+            else:
+                self.register[c] = 0
+
+    cpu = CPU(puzzle_input[1:])
+    cpu.bind_ip(int(puzzle_input[0].split()[1]))
+    while 0 <= cpu.ip < len(cpu.program):
+        if cpu.ip == 28:
+            return cpu.register[[int(x) for x in cpu.program[28].split()[1:3] if x != "0"][0]]
+        cpu.run()
+
+def main():
+    puzzle_input = util.read.as_lines()
+
+    value = solve(puzzle_input)
+
+    print("The lowest non-negative integer value that causes the program to halt with the fewest instructions is " + str(value) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    @unittest.skip("No test cases")
+    def test_ex1(self):
+        pass
+
+run(main)
---
+++ 2018/21/Part2.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2018 Day 21, Part 2
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+import colorama, time
+
+def solve(puzzle_input):
+    potential = set()
+
+    l6 = int(puzzle_input[7].split()[2])
+    l7 = int(puzzle_input[8].split()[1])
+    l8 = int(puzzle_input[9].split()[2])
+    l10 = int(puzzle_input[11].split()[2])
+    l11 = int(puzzle_input[12].split()[2])
+    l12 = int(puzzle_input[13].split()[2])
+    l13 = int(puzzle_input[14].split()[1])
+    l19 = int(puzzle_input[20].split()[2])
+
+    a, b, c, d = 0, 0, 0, 0
+    skip_to_8 = False
+    while True:
+        if not skip_to_8:
+            c = d | l6
+            d = l7
+        skip_to_8 = False
+        b = c & l8
+        d += b
+        d &= l10
+        d *= l11
+        d &= l12
+        if 256 > c:
+            if d in potential:
+                return last
+            else:
+                potential.add(d)
+                last = d
+        else:
+            b = 0
+            while True:
+                a = b + 1
+                a *= l19
+                if a > c:
+                    c = b
+                    skip_to_8 = True
+                    break
+                else:
+                    b += 1
+                    continue
+
+def main():
+    puzzle_input = util.read.as_lines()
+
+    value = solve(puzzle_input)
+
+    print("The lowest non-negative integer value that causes the program to halt with the most instructions is " + str(value) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    @unittest.skip("No test cases")
+    def test_ex1(self):
+        pass
+
+run(main)
