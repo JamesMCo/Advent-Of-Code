d08299ef5ca1d15a2ec94fef03d173a740bc0f78
James C <3661681+JamesMCo@users.noreply.github.com>
2019-12-01 08:23:21 +0000

    Completed 2019 Day 1

---
+++ 2019/01/Part1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2019 Day 1, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    return sum(int(x/3) - 2 for x in puzzle_input)
+
+def main():
+    puzzle_input = util.read.as_int_list("\n")
+
+    fuel = solve(puzzle_input)
+
+    print("The sum of the fuel requirements is " + str(fuel) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve([12]), 2)
+
+    def test_ex2(self):
+        self.assertEqual(solve([14]), 2)
+
+    def test_ex3(self):
+        self.assertEqual(solve([1969]), 654)
+
+    def test_ex4(self):
+        self.assertEqual(solve([100756]), 33583)
+
+run(main)
---
+++ 2019/01/Part2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2019 Day 1, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    def aux(n):
+        s = int(n/3) - 2
+        if s <= 0:
+            return 0
+        return s + aux(s)
+
+    return sum(aux(x) for x in puzzle_input)
+
+def main():
+    puzzle_input = util.read.as_int_list("\n")
+
+    fuel = solve(puzzle_input)
+
+    print("The sum of the fuel requirements is " + str(fuel) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(solve([14]), 2)
+
+    def test_ex2(self):
+        self.assertEqual(solve([1969]), 966)
+
+    def test_ex3(self):
+        self.assertEqual(solve([100756]), 50346)
+
+run(main)
