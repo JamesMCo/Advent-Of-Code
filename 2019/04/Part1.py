e6aa2f886546cd99b03c511e1abba1e1493fee63
James C <3661681+JamesMCo@users.noreply.github.com>
2019-12-04 05:19:11 +0000

    Completed 2019 Day 4

---
+++ 2019/04/Part1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2019 Day 4, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    def adjacency_rule(n):
+        n = str(n)
+        for i in range(len(n) - 1):
+            if n[i] == n[i+1]:
+                return True
+        return False
+
+    def increase_rule(n):
+        n = str(n)
+        return n == "".join(sorted(n))
+
+    count = 0
+    for candidate in range(puzzle_input[0], puzzle_input[1] + 1):
+        if len(str(candidate)) == 6 and adjacency_rule(candidate) and increase_rule(candidate):
+           count += 1
+    return count
+
+def main():
+    puzzle_input = [int(x) for x in util.read.as_string().split("-")]
+
+    count = solve(puzzle_input)
+
+    print("The number of passwords within the range given is " + str(count) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    @unittest.skip("No test cases")
+    def test_ex1(self):
+        pass
+
+run(main)
