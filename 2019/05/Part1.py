0acf161e2b6ae4ba40a071c1e7776c5922e47ef6
James C <3661681+JamesMCo@users.noreply.github.com>
2019-12-05 13:39:51 +0000

    Completed 2019 Day 5

---
+++ 2019/05/Part1.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2019 Day 5, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    class Intcode:
+        def __init__(self, code):
+            self.opcodes   = {1:  self.add,
+                              2:  self.mul,
+                              3:  self.inp,
+                              4:  self.out,
+                              99: self.hlt}
+            self.outstream = []
+
+            self.code      = code
+            self.ip        = 0
+            self.halted    = False
+
+        def step(self):
+            if not self.halted and (0 <= self.ip <= len(self.code)):
+                op = self.code[self.ip]
+                self.opcodes[op % 100](op)
+
+        def read(self, mode, offset):
+            value = 0
+            if mode == "0":   #Position mode
+                value = self.code[self.code[self.ip + offset]]
+            elif mode == "1": #Immediate mode
+                value = self.code[self.ip + offset]
+            else:
+                raise NotImplementedError(f"Intcode read mode {mode} not understood")
+            return value
+
+        def add(self, op):
+            o = str(op).zfill(5)
+            a = self.read(o[2], 1)
+            b = self.read(o[1], 2)
+            l = self.read("1",  3)
+            self.code[l] = a + b
+            self.ip += 4
+
+        def mul(self, op):
+            o = str(op).zfill(5)
+            a = self.read(o[2], 1)
+            b = self.read(o[1], 2)
+            l = self.read("1",  3)
+            self.code[l] = a * b
+            self.ip += 4
+
+        def inp(self, op):
+            o = str(op).zfill(3)
+            l = self.read("1",  1)
+            self.code[l] = 1
+            self.ip += 2
+
+        def out(self, op):
+            o = str(op).zfill(3)
+            v = self.read(o[0], 1)
+            self.outstream.append(v)
+            self.ip += 2
+
+        def hlt(self, op):
+            self.halted = True
+
+    i = Intcode(puzzle_input)
+    while not i.halted:
+        i.step()
+
+    return i.outstream[-1]
+
+def main():
+    puzzle_input = util.read.as_int_list(",")
+
+    diag_code = solve(puzzle_input)
+
+    print("The diagnostic code the program produces is " + str(diag_code) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        self.assertEqual(True, True)
+        # self.assertEqual(solve([]), )
+
+run(main)
