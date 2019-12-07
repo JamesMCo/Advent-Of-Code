7e9545d7d1c87639497a560cb030702db7e0a96c
James C <3661681+JamesMCo@users.noreply.github.com>
2019-12-07 04:45:09 +0000

    Added debug/trace mode to 2019 Intcode

--- 2019/05/Part1.py
+++ 2019/05/Part1.py
@@ -9,6 +9,8 @@ sys.path.append(os.path.abspath("../.."))
 import unittest, util.read
 from util.tests import run
 
+from colorama import Fore
+
 def solve(puzzle_input):
     class Intcode:
         def __init__(self, code):
@@ -23,10 +25,16 @@ def solve(puzzle_input):
             self.ip        = 0
             self.halted    = False
 
+            self.debug     = False
+
         def step(self):
             if not self.halted and (0 <= self.ip <= len(self.code)):
                 op = self.code[self.ip]
-                self.opcodes[op % 100](op)
+                try:
+                    self.opcodes[op % 100](op)
+                except KeyError:
+                    self.halted = True
+                    raise NotImplementedError(f"Intcode opcode {op} not understood")
 
         def read(self, mode, offset):
             value = 0
@@ -43,6 +51,22 @@ def solve(puzzle_input):
             a = self.read(o[2], 1)
             b = self.read(o[1], 2)
             l = self.read("1",  3)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]} {self.code[self.ip + 3]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}ADD{Fore.RESET}")
+                if o[2]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {a})")
+                elif o[2] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                print(f"{pad}   = {a} + {b} = {a + b}")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+            
             self.code[l] = a + b
             self.ip += 4
 
@@ -51,22 +75,62 @@ def solve(puzzle_input):
             a = self.read(o[2], 1)
             b = self.read(o[1], 2)
             l = self.read("1",  3)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]} {self.code[self.ip + 3]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}MUL{Fore.RESET}")
+                if o[2]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {a})")
+                elif o[2] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                print(f"{pad}   = {a} * {b} = {a * b}")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+            
             self.code[l] = a * b
             self.ip += 4
 
         def inp(self, op):
             o = str(op).zfill(3)
             l = self.read("1",  1)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}INP{Fore.RESET}")
+                print(f"{pad}   = 1")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+            
             self.code[l] = 1
             self.ip += 2
 
         def out(self, op):
             o = str(op).zfill(3)
             v = self.read(o[0], 1)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}OUT{Fore.RESET}")
+                if o[0]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {v})")
+                elif o[0] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                print(f"{pad}   = {v}")
+
             self.outstream.append(v)
             self.ip += 2
 
         def hlt(self, op):
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}HLT{Fore.RESET}")
+
             self.halted = True
 
     i = Intcode(puzzle_input)
--- 2019/05/Part2.py
+++ 2019/05/Part2.py
@@ -9,6 +9,8 @@ sys.path.append(os.path.abspath("../.."))
 import unittest, util.read
 from util.tests import run
 
+from colorama import Fore
+
 def solve(puzzle_input):
     class Intcode:
         def __init__(self, code):
@@ -27,10 +29,16 @@ def solve(puzzle_input):
             self.ip        = 0
             self.halted    = False
 
+            self.debug     = False
+
         def step(self):
             if not self.halted and (0 <= self.ip <= len(self.code)):
                 op = self.code[self.ip]
-                self.opcodes[op % 100](op)
+                try:
+                    self.opcodes[op % 100](op)
+                except KeyError:
+                    self.halted = True
+                    raise NotImplementedError(f"Intcode opcode {op} not understood")
 
         def read(self, mode, offset):
             value = 0
@@ -47,6 +55,22 @@ def solve(puzzle_input):
             a = self.read(o[2], 1)
             b = self.read(o[1], 2)
             l = self.read("1",  3)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]} {self.code[self.ip + 3]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}ADD{Fore.RESET}")
+                if o[2]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {a})")
+                elif o[2] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                print(f"{pad}   = {a} + {b} = {a + b}")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+            
             self.code[l] = a + b
             self.ip += 4
 
@@ -55,18 +79,53 @@ def solve(puzzle_input):
             a = self.read(o[2], 1)
             b = self.read(o[1], 2)
             l = self.read("1",  3)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]} {self.code[self.ip + 3]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}MUL{Fore.RESET}")
+                if o[2]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {a})")
+                elif o[2] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                print(f"{pad}   = {a} * {b} = {a * b}")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+            
             self.code[l] = a * b
             self.ip += 4
 
         def inp(self, op):
             o = str(op).zfill(3)
             l = self.read("1",  1)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}INP{Fore.RESET}")
+                print(f"{pad}   = 5")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+            
             self.code[l] = 5
             self.ip += 2
 
         def out(self, op):
             o = str(op).zfill(3)
             v = self.read(o[0], 1)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}OUT{Fore.RESET}")
+                if o[0]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {v})")
+                elif o[0] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                print(f"{pad}   = {v}")
+
             self.outstream.append(v)
             self.ip += 2
 
@@ -74,6 +133,24 @@ def solve(puzzle_input):
             o = str(op).zfill(4)
             b = self.read(o[1], 1)
             p = self.read(o[0], 2)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}JIT{Fore.RESET}")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[0]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {p})")
+                elif o[0] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                if b != 0:
+                    print(f"{pad}   = {b} != 0, jumping to address {p}")
+                else:
+                    print(f"{pad}   = {b} == 0, not jumping")
+
             if b != 0:
                 self.ip = p
             else:
@@ -83,6 +160,24 @@ def solve(puzzle_input):
             o = str(op).zfill(4)
             b = self.read(o[1], 1)
             p = self.read(o[0], 2)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}JIF{Fore.RESET}")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[0]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {p})")
+                elif o[0] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                if b == 0:
+                    print(f"{pad}   = {b} == 0, jumping to address {p}")
+                else:
+                    print(f"{pad}   = {b} != 0, not jumping")
+
             if b == 0:
                 self.ip = p
             else:
@@ -93,6 +188,25 @@ def solve(puzzle_input):
             a = self.read(o[2], 1)
             b = self.read(o[1], 2)
             l = self.read("1",  3)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}LSS{Fore.RESET}")
+                if o[2]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {a})")
+                elif o[2] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                if a < b:
+                    print(f"{pad}   = {a} < {b}, result 1")
+                else:
+                    print(f"{pad}   = {a} !< {b}, result 0")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+
             if a < b:
                 self.code[l] = 1
             else:
@@ -104,6 +218,25 @@ def solve(puzzle_input):
             a = self.read(o[2], 1)
             b = self.read(o[1], 2)
             l = self.read("1",  3)
+
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op} {Fore.GREEN}{self.code[self.ip + 1]} {self.code[self.ip + 2]}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}EQL{Fore.RESET}")
+                if o[2]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in position mode (value {a})")
+                elif o[2] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 1]}{Fore.RESET} in immediate mode")
+                if o[1]   == "0":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in position mode (value {b})")
+                elif o[1] == "1":
+                    print(f"{pad}   * {Fore.GREEN}{self.code[self.ip + 2]}{Fore.RESET} in immediate mode")
+                if a < b:
+                    print(f"{pad}   = {a} == {b}, result 1")
+                else:
+                    print(f"{pad}   = {a} != {b}, result 0")
+                print(f"{pad} - storing in {Fore.GREEN}{l}{Fore.RESET}")
+
             if a == b:
                 self.code[l] = 1
             else:
@@ -111,6 +244,11 @@ def solve(puzzle_input):
             self.ip += 4
 
         def hlt(self, op):
+            if self.debug:
+                pad = " " * (len(str(self.ip)) + 2)
+                print(f"[{Fore.CYAN}{self.ip}{Fore.RESET}] {Fore.YELLOW}{op}{Fore.RESET}")
+                print(f"{pad} - {Fore.YELLOW}HLT{Fore.RESET}")
+
             self.halted = True
 
     i = Intcode(puzzle_input)
