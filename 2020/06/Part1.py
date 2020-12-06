14051f2dbba22324372f82fb41ad830dbe8b370a
James C <3661681+JamesMCo@users.noreply.github.com>
2020-12-06 05:13:50 +0000

    Complete 2020 Day 6

---
+++ 2020/06/Part1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2020 Day 6, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    def consume_group():
+        output = []
+        while puzzle_input:
+            line = puzzle_input.pop(0)
+            if line == "":
+                break
+            output.append(set(line))
+        return output[0].union(*output[1:])
+
+    groups = []
+    while puzzle_input:
+        groups.append(consume_group())
+
+    return sum(len(g) for g in groups)
+
+def main():
+    puzzle_input = util.read.as_lines()
+
+    counts = solve(puzzle_input)
+
+    print("The sum of the number of questions to which anyone answered \"yes\" for all groups is " + str(counts) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        return self.assertEqual(solve(["abcx",
+                                       "abcy",
+                                       "abcz"]), 6)
+
+    def test_ex2(self):
+        return self.assertEqual(solve(["abc",
+                                       "",
+                                       "a",
+                                       "b",
+                                       "c",
+                                       "",
+                                       "ab",
+                                       "ac",
+                                       "",
+                                       "a",
+                                       "a",
+                                       "a",
+                                       "a",
+                                       "",
+                                       "b"]), 11)
+
+run(main)
