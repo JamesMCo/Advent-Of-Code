a78afb24eb34f307fca72b33bac396612f43cd20
James C <3661681+JamesMCo@users.noreply.github.com>
2020-12-16 15:20:56 +0000

    Fully complete 2020 Day 16 Part 2

--- 2020/16/Part2.py
+++ 2020/16/Part2.py
@@ -35,56 +35,29 @@ def solve(puzzle_input):
         return ((int(vals[0]), int(vals[1])), (int(vals[2]), int(vals[3])))
 
     def is_invalid(l):
-        return sum(x for x in l if not any(r[0] <= x <= r[1] for f in fields.values() for r in f)) >= 1
+        return any(True for x in l if not any(r[0] <= x <= r[1] for f in fields.values() for r in f))
 
     fields         = {line.split(":")[0]:parse_rules(line) for line in consume_section()}
     your_ticket    = [int(n) for n in consume_section()[1].split(",")]
     nearby_tickets = [[int(n) for n in l.split(",")] for l in consume_section()[1:] if not is_invalid([int(n) for n in l.split(",")])]
-    # with open("debug.txt", "w") as f:
-    #     f.write("\n".join(",".join(str(n) for n in l) for l in nearby_tickets))
-    # return
 
     unallocated_fields = {k:set(n for n in range(len(fields))) for k in fields}
     allocated_fields   = {}
     while len(unallocated_fields) > 0:
-        changed = False
         field_for_deletion = []
         for field in unallocated_fields:
             for col in list(unallocated_fields[field]):
                 working = [t[col] for t in nearby_tickets]
-                # print(f"Checking {working} against {fields[field]}")
                 if not all(fields[field][0][0] <= t <= fields[field][0][1] or fields[field][1][0] <= t <= fields[field][1][1] for t in working):
-                    if field == "price":
-                        for i, t in enumerate(working):
-                            if not (fields[field][0][0] <= t <= fields[field][0][1] or fields[field][1][0] <= t <= fields[field][1][1]):
-                                print(f"{i}, {t}: {fields[field][0][0] <= t <= fields[field][0][1] or fields[field][1][0] <= t <= fields[field][1][1]}")
                     unallocated_fields[field].remove(col)
-                    print(f"{col} is not a correct column for field {field}")
-                    if len(unallocated_fields[field]) == 0:
-                        input(f"{field} just removed its last possible col")
-                    changed = True
-            print(f"{field} potential matches = {unallocated_fields[field]}")
 
             if len(unallocated_fields[field]) == 1:
-                print(f"This means {field} is known!")
                 (allocated_fields[field],) = unallocated_fields[field]
                 for other in unallocated_fields:
-                    if other != field and allocated_fields[field] in unallocated_fields[other]:
-                        unallocated_fields[other].remove(allocated_fields[field])
+                    unallocated_fields[other].discard(allocated_fields[field])
                 field_for_deletion.append(field)
-                changed = True
-            print()
         for d in field_for_deletion:
             del unallocated_fields[d]
-
-        if not changed:
-        #     print("Nothing changed")
-            print(allocated_fields)
-            print(unallocated_fields)
-            exit(1)
-            # input()
-        # input()
-        # print(unallocated_fields)
 
     return prod(your_ticket[allocated_fields[field]] for field in allocated_fields if field.startswith("departure"))
 
@@ -96,18 +69,6 @@ def main():
     print("The product of the six values starting with the word departure is " + str(product) + ".")
 
 class AOC_Tests(unittest.TestCase):
-    # def test_ex1(self):
-    #     return self.assertEqual(solve(["class: 0-1 or 4-19",
-    #                                    "row: 0-5 or 8-19",
-    #                                    "seat: 0-13 or 16-19",
-    #                                    "",
-    #                                    "your ticket:",
-    #                                    "11,12,13",
-    #                                    "",
-    #                                    "nearby tickets:",
-    #                                    "3,9,18",
-    #                                    "15,1,5",
-    #                                    "5,14,9"]), 0)
     @unittest.skip("No test cases")
     def test_ex1(self):
         pass
