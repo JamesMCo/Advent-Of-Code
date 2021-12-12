11fb0ebb8dc60062b9b8fb6ee163a9182cefe222
James C <3661681+JamesMCo@users.noreply.github.com>
2021-12-12 06:18:51 +0000

    Adjust test.py to use reduce duplication of code over parts 1 and 2

--- test.py
+++ test.py
@@ -24,34 +24,24 @@ with open(f"docs/_data/{args.year}.yml") as f:
 os.chdir(str(args.year))
 
 printed = False
-p_day   = False
 for day in range(1, 26):
+    p_day = False
     if os.path.isdir(str(day).zfill(2)):
         os.chdir(str(day).zfill(2))
-        if os.path.isfile("Part1.py"):
-            if printed:
-                print("\n")
-            sep = "*" * len(f"**  Day {day}  - {names[day - 1]}  **")
-            print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Day {day}  - {names[day - 1]}  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n\n\n{colorama.Fore.GREEN}Testing Part 1{colorama.Fore.RESET}\n")
-            p_day = True
-            
-            r = os.system(s + "Part1.py")
-            if r:
-                exit(r)
+        for part in [1, 2]:
+            if os.path.isfile(f"Part{part}.py"):
+                if printed:
+                    print("\n")
+                if not p_day:
+                    sep = "*" * len(f"**  Day {day}  - {names[day - 1]}  **")
+                    print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Day {day}  - {names[day - 1]}  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n\n\n{colorama.Fore.GREEN}Testing Part {part}{colorama.Fore.RESET}\n")
+                else:
+                    print(f"{colorama.Fore.GREEN}Testing Part {part}{colorama.Fore.RESET}\n")
 
-            printed = True
-        if os.path.isfile("Part2.py"):
-            if printed:
-                print("\n")
-            if not p_day:
-                sep = "*" * len(f"**  Day {day}  - {names[day - 1]}  **")
-                print(f"{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}**  Day {day}  - {names[day - 1]}  **{colorama.Fore.RESET}\n{colorama.Fore.YELLOW}{sep}{colorama.Fore.RESET}\n\n\n{colorama.Fore.GREEN}Testing Part 2{colorama.Fore.RESET}\n")
-            else:
-                print(f"{colorama.Fore.GREEN}Testing Part 2{colorama.Fore.RESET}\n")
-            
-            r = os.system(s + "Part2.py")
-            if r:
-                exit(r)
+                r = os.system(s + f"Part{part}.py")
+                if r:
+                    exit(r)
 
-            printed = True
+                printed = True
+                p_day = True
         os.chdir("..")
