255da6dff41342e47841f00661896e9400810b01
James C <3661681+JamesMCo@users.noreply.github.com>
2021-12-25 07:00:17 +0000

    Adjust 2D World to support item assignment

--- util/two_d_world.py
+++ util/two_d_world.py
@@ -56,6 +56,9 @@ class World:
     def __getitem__(self, key):
         return self.grid[key]
 
+    def __setitem__(self, key, value):
+        self.grid[key] = value
+
     def in_bounds(self, x, y):
         return self.min_x <= x <= self.max_x and self.min_y <= y <= self.max_y
 
