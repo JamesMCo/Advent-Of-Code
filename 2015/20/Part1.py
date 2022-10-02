bbef41df3a68a1cb52cb29a7fe6e42aa4a0232f7
James C <3661681+JamesMCo@users.noreply.github.com>
2022-10-02 02:44:32 +0100

    Complete 2015 Day 20

---
+++ 2015/20/Part1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2015 Day 20, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+def solve(puzzle_input):
+    def factors(n):
+        pairs = [(f, int(n/f)) for f in range(1, int(n**0.5 + 1)) if n % f == 0]
+        unique = set()
+        for pair in pairs:
+            unique.add(pair[0])
+            unique.add(pair[1])
+        return list(unique)
+
+    i = 1
+    while sum(factors(i)) * 10 < puzzle_input:
+        i += 1
+    return i
+
+def main():
+    puzzle_input = util.read.as_int()
+
+    house = solve(puzzle_input)
+
+    print("The lowest house number that gets at least as many presents as the puzzle input is " + str(house) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    @unittest.skip("No test cases")
+    def test_ex1(self):
+        pass
+
+run(main)
