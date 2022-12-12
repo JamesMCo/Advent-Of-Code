350303a39cf3ef2d3775d9889896f387e985d332
James C <3661681+JamesMCo@users.noreply.github.com>
2022-12-12 15:36:57 +0000

    Complete 2022 Day 12

---
+++ 2022/12/Part1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2022 Day 12, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+import networkx as nx
+
+def solve(puzzle_input):
+    height = len(puzzle_input)
+    width  = len(puzzle_input[0])
+
+    def parse_space(space):
+        match space:
+            case "S":
+                return 0
+            case "E":
+                return 25
+            case letter:
+                return ord(letter) - ord("a")
+
+    start = None
+    target = None
+    for y, row in enumerate(puzzle_input):
+        for x, col in enumerate(row):
+            if col == "S":
+                start = (x, y)
+                if target != None:
+                    break
+            if col == "E":
+                target = (x, y)
+                if start != None:
+                    break
+
+    puzzle_input = [[parse_space(space) for space in row] for row in puzzle_input]
+
+    def neighbours(x, y):
+        # Returns in order up, right, down, left
+        if y != 0:
+            yield ((x, y - 1), puzzle_input[y - 1][x])
+        if x != width - 1:
+            yield ((x + 1, y), puzzle_input[y][x + 1])
+        if y != height - 1:
+            yield ((x, y + 1), puzzle_input[y + 1][x])
+        if x != 0:
+            yield ((x - 1, y), puzzle_input[y][x - 1])
+
+    heightmap_graph = nx.DiGraph()
+
+    for y, row in enumerate(puzzle_input):
+        for x, strength in enumerate(row):
+            for neighbour, neighbour_strength in neighbours(x, y):
+                if neighbour_strength <= strength + 1:
+                    heightmap_graph.add_edge((x, y), neighbour)
+
+    return nx.shortest_path_length(heightmap_graph, start, target)
+
+def main():
+    puzzle_input = util.read.as_lines()
+
+    steps = solve(puzzle_input)
+
+    print("The fewest steps required to reach the location with the best signal is " + str(steps) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        return self.assertEqual(solve(["Sabqponm",
+                                       "abcryxxl",
+                                       "accszExk",
+                                       "acctuvwj",
+                                       "abdefghi"]), 31)
+
+run(main)
