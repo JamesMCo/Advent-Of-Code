a4f9879136e8414fb3d3cbd8a1e9d643fdaba4c8
James C <3661681+JamesMCo@users.noreply.github.com>
2022-12-20 16:40:18 +0000

    Complete 2022 Day 20

---
+++ 2022/20/Part1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2022 Day 20, Part 1
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+from collections import deque
+
+def solve(puzzle_input):
+    class Number:
+        def __init__(self, val):
+            self.val = val
+
+        def mix(self, l):
+            l.rotate(-1-l.index(self))
+            l.pop()
+            l.rotate(-self.val)
+            l.append(self)
+
+        def move_to_start(self, l):
+            l.rotate(-l.index(self))
+    
+    puzzle_input = [Number(n) for n in puzzle_input]
+    file = deque(puzzle_input)
+    for n in puzzle_input:
+        n.mix(file)
+
+    # Rotate such that 0 is at index 0
+    for n in puzzle_input:
+        if n.val == 0:
+            n.move_to_start(file)
+
+    ns = len(puzzle_input)
+    return file[1000 % ns].val + file[2000 % ns].val + file[3000 % ns].val
+
+def main():
+    puzzle_input = util.read.as_int_list("\n")
+
+    grove_coordinates = solve(puzzle_input)
+
+    print("The sum of the three numbers that form the grove coordinates is " + str(grove_coordinates) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        return self.assertEqual(solve([1, 2, -3, 3, -2, 0, 4]), 3)
+
+run(main)
