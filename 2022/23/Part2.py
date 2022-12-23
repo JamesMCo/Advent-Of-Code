9c61ad2a4fc8afca09bebfbde1ae01c174c02dcb
James C <3661681+JamesMCo@users.noreply.github.com>
2022-12-23 15:53:18 +0000

    Complete 2022 Day 23

---
+++ 2022/23/Part2.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+#Advent of Code
+#2022 Day 23, Part 2
+#Solution by James C. (https://github.com/JamesMCo)
+
+import os, sys
+sys.path.append(os.path.abspath("../.."))
+import unittest, util.read
+from util.tests import run
+
+from collections import defaultdict
+from math import inf
+
+def solve(puzzle_input):
+    class Elf:
+        elves = []
+        positions = set()
+        proposals = defaultdict(int)
+
+        directions = [0, 1, 2, 3]
+        round_num = 0
+
+        @classmethod
+        def round(cls):
+            # Proposal phase
+            for elf in cls.elves:
+                elf.propose()
+
+            # Movement phase
+            cls.positions = set()
+            cls.min_x = cls.min_y = inf
+            cls.max_x = cls.max_y = -inf
+            elf_moved = False
+            for elf in cls.elves:
+                this_elf_moved = elf.move()
+                elf_moved = elf_moved or this_elf_moved
+
+                Elf.positions.add((elf.x, elf.y))
+
+            # Cleanup
+            cls.proposals.clear()
+            cls.round_num += 1
+
+            return elf_moved
+
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            self.proposal = None
+
+            Elf.elves.append(self)
+            Elf.positions.add((x, y))
+
+        def get_neighbours(self):
+            return [neighbour for neighbour in [(self.x + dx, self.y + dy) for dx in range(-1, 2) for dy in range(-1, 2) if not (dx == 0 and dy == 0)] if neighbour in Elf.positions]
+
+        def register_proposal(self, next_x, next_y):
+            self.proposal = (next_x, next_y)
+            Elf.proposals[(next_x, next_y)] += 1
+
+        def propose(self):
+            if not (neighbours := self.get_neighbours()):
+                return
+
+            for dir_check in range(4):
+                match Elf.directions[(Elf.round_num + dir_check) % 4]:
+                    case 0:
+                        # North
+                        if all(not (self.x + dx, self.y - 1) in neighbours for dx in range(-1, 2)):
+                            return self.register_proposal(self.x, self.y - 1)
+                    case 1:
+                        # South
+                        if all(not (self.x + dx, self.y + 1) in neighbours for dx in range(-1, 2)):
+                            return self.register_proposal(self.x, self.y + 1)
+                    case 2:
+                        # West
+                        if all(not (self.x - 1, self.y + dy) in neighbours for dy in range(-1, 2)):
+                            return self.register_proposal(self.x - 1, self.y)
+                    case 3:
+                        # East
+                        if all(not (self.x + 1, self.y + dy) in neighbours for dy in range(-1, 2)):
+                            return self.register_proposal(self.x + 1, self.y)
+
+        def move(self):
+            moving = False
+            if self.proposal != None and Elf.proposals[self.proposal] == 1:
+                self.x, self.y = self.proposal
+                moving = True
+            self.proposal = None
+            return moving
+
+    for y, row in enumerate(puzzle_input):
+        for x, col in enumerate(row):
+            if col == "#":
+                Elf(x, y)
+
+    r = 1
+    while True:
+        if not Elf.round():
+            return r
+        r += 1
+
+def main():
+    puzzle_input = util.read.as_lines()
+
+    r = solve(puzzle_input)
+
+    print("The number of the first round where no elf moves is " + str(r) + ".")
+
+class AOC_Tests(unittest.TestCase):
+    def test_ex1(self):
+        return self.assertEqual(solve(["....#..",
+                                       "..###.#",
+                                       "#...#.#",
+                                       ".#...##",
+                                       "#.###..",
+                                       "##.#.##",
+                                       ".#..#.."]), 20)
+
+run(main)
