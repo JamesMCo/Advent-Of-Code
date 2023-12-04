049ae534e9f80f8af5208e4219ecc0d713e7cd9e
James C. <3661681+JamesMCo@users.noreply.github.com>
2023-12-04 02:21:53 +0000

    Add type annotations to util.read

--- util/read.py
+++ util/read.py
@@ -1,27 +1,27 @@
-def as_string():
+def as_string() -> str:
     with open("puzzle_input.txt") as f:
         return f.read().strip()
 
-def as_lines():
+def as_lines() -> list[str]:
     with open("puzzle_input.txt") as f:
         return f.read().strip().split("\n")
 
-def as_lines_sans_ending_line():
+def as_lines_sans_ending_line() -> list[str]:
     with open("puzzle_input.txt") as f:
         return f.read()[:-1].split("\n")
 
-def as_lines_only_rstrip():
+def as_lines_only_rstrip() -> list[str]:
     with open("puzzle_input.txt") as f:
         return f.read().rstrip().split("\n")
 
-def as_string_list(delim):
+def as_string_list(delim) -> list[str]:
     with open("puzzle_input.txt") as f:
         return f.read().strip().split(delim)
 
-def as_int():
+def as_int() -> int:
     with open("puzzle_input.txt") as f:
         return int(f.read().strip())
 
-def as_int_list(delim):
+def as_int_list(delim) -> list[int]:
     with open("puzzle_input.txt") as f:
         return [int(x) for x in f.read().strip().split(delim)]
