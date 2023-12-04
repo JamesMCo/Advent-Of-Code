5328ff2a395ff424fc94506cc96d07c2084db738
James C. <3661681+JamesMCo@users.noreply.github.com>
2023-12-04 03:37:15 +0000

    Refactor test script
    - Represent each day with a class
    - Use the day class' context manager to handle changing in to and out of the day's directory, and use __enter__ as the moment to check whether Part 1/2 exist and try to download the input if missing
    - Refactor duration formatting in GitHub summary and stdout to use decimal.Decimal

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import argparse, collections, itertools, os, requests, time, yaml
+import argparse, collections, itertools, os, requests, time, types, typing as t, yaml
+from decimal import Decimal
 from util.colour import *
 
 if time.gmtime().tm_mon == 12:
@@ -12,10 +13,76 @@ parser = argparse.ArgumentParser(description="Runs either the current year's or 
 parser.add_argument("year", help="specify which year to test", nargs="?", default=current_year)
 args = parser.parse_args()
 
-if os.name == "nt":
-    s = ""
-else:
-    s = "python ./"
+class Day:
+    day: int
+    day_str: str
+
+    dir_exists:   t.Optional[bool]
+    input_exists: t.Optional[bool]
+    p1_exists:    t.Optional[bool]
+    p2_exists:    t.Optional[bool]
+
+    def __init__(self: t.Self, day: int) -> None:
+        self.day = day
+        self.day_str = f"{self.day:0>2}"
+
+        self.dir_exists   = None
+        self.input_exists = None
+        self.p1_exists    = None
+        self.p2_exists    = None
+
+    def __enter__(self: t.Self) -> t.Self:
+        if os.path.isdir(self.day_str):
+            os.chdir(self.day_str)
+            self.dir_exists = True
+
+            self.p1_exists = os.path.isfile(f"Part1.py")
+            self.p2_exists = os.path.isfile(f"Part2.py")
+
+            self.download_input_if_missing()
+        else:
+            self.dir_exists = False
+        return self
+
+    def __exit__(self: t.Self, exc_type: t.Optional[t.Type[BaseException]], exc_val: t.Optional[BaseException], exc_tb: t.Optional[types.TracebackType]) -> bool | t.NoReturn:
+        if self.dir_exists:
+            os.chdir("..")
+        return False
+
+    def download_input_if_missing(self: t.Self) -> None:
+        if os.path.isfile("puzzle_input.txt"):
+            self.input_exists = True
+        else:
+            if "jamdroid_input_cache_key" in os.environ:
+                r = requests.get(f"https://jamdro.id/api/adventofcode/input/{args.year}/{self.day_str}", headers={"Authorization": f"Bearer {os.environ["jamdroid_input_cache_key"]}"})
+                if r.status_code == 200:
+                    with open("puzzle_input.txt", "w") as f:
+                        # Ensure only one newline at end of file
+                        # Can't use strip, in case some whitespace at end of final line
+                        file_lines = r.text.split("\n")
+                        file_lines = list(itertools.dropwhile(lambda l: l == "", file_lines[::-1]))[::-1]
+                        f.write("\n".join(file_lines) + "\n")
+
+                        self.input_exists = True
+                        return
+            self.input_exists = False
+
+    def print_header(self: t.Self) -> None:
+        sep = "*" * len(f"**  Day {self.day}  - {names[self.day - 1]}  **")
+        print(f"{yellow(f"{sep}\n**  Day {self.day}  - {names[self.day - 1]}  **\n{sep}")}\n\n")
+
+    def can_run_any_part(self: t.Self) -> bool:
+        return self.can_run_part(1) or self.can_run_part(2)
+
+    def can_run_part(self: t.Self, part_num: int) -> bool:
+        match part_num:
+            case 1: return self.dir_exists and self.input_exists and self.p1_exists
+            case 2: return self.dir_exists and self.input_exists and self.p2_exists
+
+    @staticmethod
+    def run_part(part_num: int) -> t.Optional[bool]:
+        print(green(f"Testing Part {part_num}\n"))
+        return os.system(f"{"" if os.name == "nt" else "python ./"}Part{part_num}.py") != 0
 
 with open(f"docs/_data/{args.year}.yml") as f:
     names = [day["name"] for day in sorted([full_day for full_day in yaml.safe_load(f)], key=lambda x: x["day"] if isinstance(x["day"], int) else 26)]
@@ -24,58 +91,33 @@ open("times.txt", "w").close()
 
 os.chdir(str(args.year))
 
-printed = False
-failed  = False
-for day in range(1, 26):
-    p_day = False
-    if os.path.isdir(str(day).zfill(2)):
-        os.chdir(str(day).zfill(2))
-        if not os.path.isfile("puzzle_input.txt"):
-            if "jamdroid_input_cache_key" in os.environ:
-                r = requests.get(f"https://jamdro.id/api/adventofcode/input/{args.year}/{day:0>2}", headers={"Authorization": f"Bearer {os.environ["jamdroid_input_cache_key"]}"})
-                if r.status_code == 200:
-                    with open("puzzle_input.txt", "w") as f:
-                        # Ensure only one newline at end of file
-                        # Can't use strip, in case some whitespace at end of final line
-                        file_lines = r.text.split("\n")
-                        file_lines = list(itertools.dropwhile(lambda l: l == "", file_lines[::-1]))[::-1]
-                        f.write("\n".join(file_lines) + "\n")
-                else:
-                    continue
-            else:
-                continue
-        for part in [1, 2]:
-            if os.path.isfile(f"Part{part}.py"):
-                if printed:
-                    print("\n")
-                if not p_day:
-                    sep = "*" * len(f"**  Day {day}  - {names[day - 1]}  **")
-                    print(f"{yellow(f"{sep}\n**  Day {day}  - {names[day - 1]}  **\n{sep}")}\n\n\n{green(f"Testing Part {part}")}\n")
-                else:
-                    print(green(f"Testing Part {part}\n"))
-                
-                with open("../../times.txt", "a") as f:
-                    f.write(f"{day}-{part}-")
+failed = False
+for d in range(1, 26):
+    with Day(d) as day:
+        if day.can_run_any_part():
+            day.print_header()
 
-                if os.system(s + f"Part{part}.py"):
-                    failed = True
+            for part in (1, 2):
+                if day.can_run_part(part):
                     with open("../../times.txt", "a") as f:
-                        f.write("fail\n")
+                        f.write(f"{day}-{part}-")
+                    if day.run_part(part):
+                        failed = True
+                        with open("../../times.txt", "a") as f:
+                            f.write("fail\n")
+                    print("\n")
 
-                printed = True
-                p_day = True
-        os.chdir("..")
 os.chdir("..")
 
 with open("times.txt") as f:
-    duration = 0
+    total_duration = 0
     durations = collections.defaultdict(lambda: ("Not attempted", "Not attempted"))
 
     for line in f.read().strip().split("\n"):
         day, part, dur = line.split("-")
         if dur != "fail":
-            duration += int(dur)
-            dur = str(round(int(dur) / 1_000_000_000, 3))
+            total_duration += int(dur)
+            dur = Decimal(int(dur) / 1_000_000_000).quantize(Decimal("0.001"))
         else:
             dur = "Fail"
         if part == "1":
@@ -85,15 +127,14 @@ with open("times.txt") as f:
     if "25" in durations:
         durations["25"] = (durations["25"][0], "N/A")
 
-    duration = str(round(duration / 1_000_000_000, 3))
-    duration += "0" * (3 - len(duration.split(".")[1]))
+    total_duration = Decimal(total_duration / 1_000_000_000).quantize(Decimal("0.001"))
 
     if github_summary := os.getenv("GITHUB_STEP_SUMMARY"):
         with open(github_summary, "w") as g:
-            g.write(f"# {current_year} Solution Runtimes\nAll{" non-failed" if failed else ""} solutions found in {duration}s.\n| Day | Part 1 | Part 2|\n|-----|--------|-------|\n")
+            g.write(f"# {current_year} Solution Runtimes\nAll{" non-failed" if failed else ""} solutions found in {total_duration}s.\n| Day | Part 1 | Part 2|\n|-----|--------|-------|\n")
             for day, parts in sorted(durations.items(), key=lambda x: int(x[0])):
                 g.write(f"| [{day}{" 🎂" if day == "9" else ""}](https://mrjamesco.uk/Advent-Of-Code/?{current_year}-{day:0>2}) | {parts[0]}{"s" if parts[0] not in ["Fail", "N/A"] else ""} | {parts[1]}{"s" if parts[1] not in ["Fail", "N/A"] else ""} |\n")
 
-    print(f"\n{cyan(f"All{" non-failed" if failed else ""} solutions found in")} {green(f"{duration}s")}{cyan(".")}")
+    print(f"\n{cyan(f"All{" non-failed" if failed else ""} solutions found in")} {green(f"{total_duration}s")}{cyan(".")}")
 
 os.remove("times.txt")
