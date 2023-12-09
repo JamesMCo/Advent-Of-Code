2e7a75c0704d01f9433042d2cc0794dcb3ebf818
James C. <3661681+JamesMCo@users.noreply.github.com>
2023-12-09 00:20:32 +0000

    Refactor 2023 Day 8 Part 2
    Refactored following reading some discussions on the Advent of Code subreddit about the assumptions that can be made for puzzle inputs. This means, even more so than before, that this solution wouldn't work for any arbitrary graph, but for those given as puzzle inputs, it should be fine.

--- 2023/08/Part2.py
+++ 2023/08/Part2.py
@@ -25,20 +25,29 @@ def solve(puzzle_input: list[str]) -> int:
         graph.add_edge(node, right)
 
     def find_cycle(start_node: str) -> int:
-        # I'm not 100% sure that this would work for all inputs,
-        # and I'm almost certain that it wouldn't work with a
-        # general input, but it works for my input at the very least!
+        # After reading through some discussions on the subreddit, I found that
+        # more assumptions could be made about the graphs given as inputs:
+        #
+        # - The routes given by the inputs will always cycle
+        # - The cycle will only contain one **Z node
+        # - The cycle for a given **A node is not shared
+        #   by any other **A node
+        # - The length from the **A node to the **Z is the
+        #   same as the length of **Z to **Z in the cycle
+        #
+        # These assumptions mean that the approach to finding the cycle is
+        # simpler than I originally implemented. Instead, we can find the
+        # number of steps needed to reach the **Z node and return that.
+        # Obviously, this wouldn't work for any arbitrary graph, but for
+        # those given as puzzle inputs, it should be fine.
 
-        instructions = cycle(zip(puzzle_input[0], range(len(puzzle_input[0]))))
+        instructions = cycle(puzzle_input[0])
         current_node = start_node
-        seen = []
-        while True:
-            current_instruction, instruction_i = next(instructions)
-            if (current_node, instruction_i) in seen:
-                return len(seen) - seen.index((current_node, instruction_i))
-            else:
-                seen.append((current_node, instruction_i))
-                current_node = graph.nodes[current_node][current_instruction]
+        steps = 0
+        while not current_node.endswith("Z"):
+            current_node = graph.nodes[current_node][next(instructions)]
+            steps += 1
+        return steps
 
     return lcm(*map(find_cycle, filter(lambda n: n.endswith("A"), graph)))
 
