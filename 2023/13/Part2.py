9f3cb2262cdb5f4d141916d8b619c890ebdffbba
James C. <3661681+JamesMCo@users.noreply.github.com>
2023-12-14 01:25:08 +0000

    Refactor 2023 Day 13 Part 2 to use a more efficient solution

--- 2023/13/Part2.py
+++ 2023/13/Part2.py
@@ -12,33 +12,28 @@ from util.tests import run
 import typing as t
 
 def solve(puzzle_input: list[str]) -> int:
-    def find_reflection(pattern: list[str], original_reflection: t.Optional[int] = None) -> int:
+    # Refactored after reading some discussions on the Advent of Code subreddit to use a different approach
+    #
+    # Before, all cells in the grid were considered to be able to be
+    # a smudge, and, the part 1 solution was run on every un-smudged grid.
+    #
+    # Now, a reflection is considered valid if there is exactly one
+    # different cell across the axis of reflection. (Part 1 could
+    # be refactored to check for exactly zero differences.) This
+    # reduces the runtime from ~0.5s to ~0.012s on my machine.
+
+    def find_reflection(pattern: list[str]) -> int:
         def is_reflection(n: int, pattern_candidate: list[str]) -> bool:
-            return all(a == b for line in pattern_candidate for a, b in zip(line[:n][::-1], line[n:]))
+            return sum(0 if a == b else 1 for line in pattern_candidate for a, b in zip(line[:n][::-1], line[n:])) == 1
 
         for x in range(1, len(pattern[0])):
-            if is_reflection(x, pattern) and x != original_reflection:
+            if is_reflection(x, pattern):
                 return x
         transposed_pattern = ["".join(row[col] for row in pattern) for col in range(len(pattern[0]))]
         for y in range(1, len(pattern)):
-            if is_reflection(y, transposed_pattern) and y * 100 != original_reflection:
+            if is_reflection(y, transposed_pattern):
                 return 100 * y
         return 0
-
-    def get_smudges(pattern: list[str]) -> t.Iterable[list[str]]:
-        for y in range(len(pattern)):
-            for x in range(len(pattern[0])):
-                output = pattern[:y]
-                output.append(pattern[y][:x] + ("." if pattern[y][x] == "#" else "#") + pattern[y][x+1:])
-                output.extend(pattern[y+1:])
-                yield output
-
-    def find_new_reflection(pattern: list[str]) -> int:
-        original_reflection = find_reflection(pattern)
-
-        for new_pattern in get_smudges(pattern):
-            if new_reflection := find_reflection(new_pattern, original_reflection):
-                return new_reflection
 
     def get_patterns() -> t.Iterable[list[str]]:
         output = []
@@ -50,7 +45,7 @@ def solve(puzzle_input: list[str]) -> int:
                 output = []
         yield output
 
-    return sum(map(find_new_reflection, get_patterns()))
+    return sum(map(find_reflection, get_patterns()))
 
 def main() -> tuple[str, int]:
     puzzle_input = util.read.as_lines()
