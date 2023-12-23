076b1b5b87ca8a8b661bb1638a8d7a09a44ba4f9
James C. <3661681+JamesMCo@users.noreply.github.com>
2023-12-23 23:51:39 +0000

    Refactor 2023 Day 23 Part 2 to be slightly more efficient building graph and not use networkx

--- 2023/23/Part2.py
+++ 2023/23/Part2.py
@@ -10,7 +10,6 @@ import unittest, util.read
 from util.tests import run
 
 from collections import deque
-import networkx as nx
 
 def solve(puzzle_input: list[str]) -> int:
     width = len(puzzle_input[0])
@@ -26,33 +25,36 @@ def solve(puzzle_input: list[str]) -> int:
                 output.add((x + dx, y + dy))
         return output
 
-    def build_graph() -> nx.Graph:
-        output = nx.Graph()
+    def build_graph() -> dict[tuple[int, int], dict[tuple[int, int], int]]:
+        output = {}
         # Find all junctions (& start/target)
         for y, row in enumerate(puzzle_input):
             for x, col in enumerate(row):
                 if col != "#":
                     if len(neighbours(x, y)) > 2 or (x, y) in (start, target):
-                        output.add_node((x, y))
+                        output[(x, y)] = {}
         # Find paths between junctions
-        for node in output.nodes:
+        for junction in output:
             queue: deque[tuple[tuple[int, int], set[tuple[int, int]]]] = deque()
-            queue.extend([(neighbour, {node}) for neighbour in neighbours(*node)])
-            while queue:
-                from_coord, seen = queue.popleft()
-                while True:
-                    next_cells = neighbours(*from_coord) - seen
-                    if len(next_cells) == 0:
-                        # If 0 next cells, then we're at a dead end. We don't need to consider this path.
-                        break
-                    if len(next_cells) == 1:
-                        # If only one option, skip immediately to consider it
-                        seen |= {from_coord}
-                        (from_coord,) = next_cells
-                    elif len(next_cells) >= 2 and from_coord != node:
-                        # Found a junction that isn't the one we're originating from
-                        output.add_edge(node, from_coord, weight=len(seen))
-                        break
+            junction_neighbours = neighbours(*junction)
+            if len(junction_neighbours) != len(output[junction]):
+                # If not already found all neighbouring paths
+                queue.extend([(neighbour, {junction}) for neighbour in junction_neighbours])
+                while queue:
+                    from_coord, seen = queue.popleft()
+                    while True:
+                        next_cells = neighbours(*from_coord) - seen
+                        if len(next_cells) == 0:
+                            # If 0 next cells, then we're at a dead end. We don't need to consider this path.
+                            break
+                        if len(next_cells) == 1:
+                            # If only one option, skip immediately to consider it
+                            seen |= {from_coord}
+                            (from_coord,) = next_cells
+                        elif len(next_cells) >= 2 and from_coord != junction:
+                            # Found a junction that isn't the one we're originating from
+                            output[junction][from_coord] = output[from_coord][junction] = len(seen)
+                            break
         return output
 
     graph = build_graph()
@@ -67,12 +69,12 @@ def solve(puzzle_input: list[str]) -> int:
             longest_length = max(longest_length, length)
             break
 
-        next_cells = list(graph.neighbors(from_coord))
+        next_cells = list(graph[from_coord])
         if target in next_cells:
             # If can go to the target, *must* go to the target (otherwise block it off)
-            longest_length = max(longest_length, length + graph[from_coord][target]["weight"])
+            longest_length = max(longest_length, length + graph[from_coord][target])
         else:
-            queue.extend((next_cell, seen | {from_coord}, length + graph[from_coord][next_cell]["weight"]) for next_cell in next_cells if next_cell not in seen)
+            queue.extend((next_cell, seen | {from_coord}, length + graph[from_coord][next_cell]) for next_cell in next_cells if next_cell not in seen)
 
     return longest_length
 
